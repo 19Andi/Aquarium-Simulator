051192a76cc401f5bff651c4076cd34b68dae274
Ioan-Octavian Teulea <ioan.teulea@student.unitbv.ro>
2023-12-09 19:24:24 +0200

    added set,reset and updateCameraVectors functions

--- Aquarium-Simulator/Camera.cpp
+++ Aquarium-Simulator/Camera.cpp
@@ -2,5 +2,43 @@
 
 Camera::Camera(const int width, const int height, const glm::vec3& position)
 {
+	startPosition = position;
+	Set(width, height, position);
+}
 
+void Camera::Set(const int width, const int height, const glm::vec3& position)
+{
+    this->isPerspective = true;
+    this->yaw = YAW;
+    this->pitch = PITCH;
+
+    this->FoVy = FOV;
+    this->width = width;
+    this->height = height;
+    this->zNear = zNEAR;
+    this->zFar = zFAR;
+
+    this->worldUp = glm::vec3(0, 1, 0);
+    this->position = position;
+
+    lastX = width / 2.0f;
+    lastY = height / 2.0f;
+    bFirstMouseMove = true;
+
+    UpdateCameraVectors();
+}
+void Camera::Reset(const int width, const int height)
+{
+    Set(width, height, startPosition);
+}
+void Camera::UpdateCameraVectors()
+{
+    // Calculate the new forward vector
+    this->forward.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    this->forward.y = sin(glm::radians(pitch));
+    this->forward.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    this->forward = glm::normalize(this->forward);
+    // Also re-calculate the Right and Up vector
+    right = glm::normalize(glm::cross(forward, worldUp));  // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+    up = glm::normalize(glm::cross(right, forward));
 }
